fe40b941910e47d9b3855d90cbe5ba828c40bdfb
Sebastian <sebastian.reyes63@inacapmail.cl>
2024-11-24 18:57:12 -0300

    Cambios para render

--- webtaller/settings.py
+++ webtaller/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJ_SECRET_KEY')
+SECRET_KEY = 'django-insecure-()hrtozcjtti$f)t)o%xzvyf_=taa+g)@!9c+n$k2)b^bh(43o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
