4aecae55a994359e9bcde093dfca5358d5e77270
Sebastian <sebastian.reyes63@inacapmail.cl>
2024-11-25 13:26:25 -0300

    Se añadio documentacion

--- test_api/views.py
+++ test_api/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-from rest_framework import viewsets
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import viewsets, status, permissions, serializers
 from .models import User, Taller, Servicio, Review, ReviewFeedback
 from .serializers import UserSerializer, TallerSerializer,ServicioSerializer, ReviewSerializer, ReviewFeedbackSerializer
 from .permissions import IsAdminUser
 from rest_framework.permissions import AllowAny, IsAdminUser
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -68,5 +69,24 @@ class ReviewFeedbackViewSet(viewsets.ModelViewSet):
             permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
 
-
-    
+class ContactForm(serializers.Serializer):
+      # simple serializer for emails
+    email = serializers.EmailField()
+    message = serializers.CharField()
+# simple endpoint to take the serializer data
+class SendEmail(APIView):
+      # permission class set to be unauthenticated
+    permission_classes = (permissions.AllowAny,)
+    # this is where the drf-yasg gets invoked
+    @swagger_auto_schema(request_body=ContactForm)
+    def post(self, request):
+          # serializer object
+        serializer = ContactForm(data=request.data)
+        # checking for errors
+        if serializer.is_valid():
+            json = serializer.data
+            return Response(
+                data={"status": "OK", "message": json},
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
